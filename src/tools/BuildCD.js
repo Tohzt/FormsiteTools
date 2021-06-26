b425337c6e7f9b70a9c80cdbca49aa059eb2f3dc
Tohzt <Steven.G.French@live.com>
2021-06-26 17:26:35 -0600

    CD_AddItems

--- src/tools/BuildCD.js
+++ src/tools/BuildCD.js
@@ -28,3 +28,55 @@ function generate_cd() {
 	}
 }
 
+// DISABLE PART OF FORM(s)
+function form_disable() {
+	for(i=0; i<document.form.elements.length; i++) {
+		document.form.elements[i].disabled=!document.form.elements[i].disabled;
+	}
+	//document.getElementById("input-numGuests").style.backgroundColor = "#CCCCCC";
+}
+
+// INCREASE / DECREASE ITEM LIST
+function inc_itemList() {
+	// IDENTIFY LIST
+	var list = document.getElementById('item-list');
+	// CREATE NEW INPUT
+	var newItem = document.createElement("input");
+	// SET NEW INPUT ID
+	newItem.setAttribute('id', 'input-item');
+
+	//newItem.innerText = "test";
+	
+	// ADD NEW ITEM TO LSIT
+	list.appendChild(newItem);
+	// STOP SUBMISSION
+	return false; 
+}
+function dec_itemList() {
+	// IDENTIFY LIST
+	var list = document.getElementById('item-list');
+	
+	/**ERROR DETECTION**/
+
+	// REMOVE ITEM FROM LIST
+	list.removeChild(list.lastChild);
+
+	// STOP SUBMISSION
+	return false; 
+}
+
+// PRINT LIST OF ITEMS
+function print_items() {
+	var list = document.getElementById('item-list');
+	var ans = "";
+	var items = list.getElementsByTagName('*');
+
+	// LOOP THROUGH LIST
+	var i, e, d;
+	for (i = 0; i < items.length; ++i) {
+		e = items[i];
+		ans = ans.concat(e.value.toString());
+	}
+
+	console.log(ans);
+}
