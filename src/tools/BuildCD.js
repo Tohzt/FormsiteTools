bdd737cf602b5dc5a92e723c7ec7a330579493eb
Tohzt <Steven.G.French@live.com>
2021-06-26 22:41:47 -0600

    cd_Generates_noFormat

--- src/tools/BuildCD.js
+++ src/tools/BuildCD.js
@@ -1,25 +1,17 @@
-function tool_addMenuItemsCD() {
-	console.log("No friggin way");
-}
-
 function generate_cd() {
-	//Init File Handling
-	//var $ = require('jquery');
-	//var fs = require('fs');
-	//var filename = './src/tools/div_FrameUp.txt';
-
 	// Init Variables
+	var cd_data = "";
 	var guest_current = 1;
 	var guest_count = document.getElementById('input-numGuests').value;
 
-	// Loop Through All Guests (guest_count)
+	// LOOP THROUGH GUESTS (guest_count)
 	for (var i = 1; i <= guest_count; i++) {
 
-		// Init Preleaded Text (RFF Eventually..)
-		var str_1 = "{{IfG#}}{{G#Name}}{{G#Colon}}";
+		// INIT STRING (RFF Eventually..)
+		var str = "{{IfG#}}{{G#Name}}{{G#Colon}}";
 
-		// Add Guest Number to CD-head
-		str_1 = str_1.replace(/#/g,guest_current.toString());
+		// ADD GUEST NUMBER TO INITIAL STRING
+		str = str.replace(/#/g,guest_current.toString());
 		
 		// GET LIST OF INPUTS
 		var list = document.getElementById('item-list');
@@ -28,21 +20,39 @@ function generate_cd() {
 		// LOOP THROUGH LIST
 		if (items.length > 0) {
 			for (var j = 0; j < items.length; ++j) {
-				console.log(j);
-				console.log(items.length);
-				str_1 = str_1.concat("{{G", i.toString(), "_", items[j].value.toString(), "}}");
+				str = str.concat(
+					"{{G",
+					guest_current,
+					"_",
+					document.getElementById('input-menu').value.toString(),
+					"_",
+					items[j].value.toString(),
+					"}}");
 			}
 		}
 
-		/** ADD MENU ITEMS **/
-		//str_1 = str_1.concat("{{G", i.toString(), "_", get_items(), "}}");
+		// ADD 'SPECIAL' INFORMATION
+		str = str.concat(
+			"{{G",
+			guest_current,
+			"<b>_SpecialHeader}}{{G</b>",
+			guest_current,
+			"_Special}}{{G",
+			guest_current,
+			"_Slash}}\n"
+		);
 
-		console.log(str_1);
-		console.log("");
+		// UPDATE
+		cd_data = cd_data.concat(str);
 
-		// Procede to next Guest
+		// PROCEED TO NEXT GUEST
 		guest_current++;
 	}
+	
+	// DISPLAY RESULTS
+	document.getElementById('preview-cd').innerHTML = cd_data;
+	// PRINT RESULT
+	console.log(cd_data);
 }
 
 
